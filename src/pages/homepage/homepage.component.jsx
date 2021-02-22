23e5f32c53dc26b294394cd80752f17d81897c6c
Luiz Gustavo <lgpossatto@gmail.com>
2021-02-22 11:39:20 -0300

    created basic structure folders, created menu-item, directory and homepage components with styles

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import "./homepage.styles.scss";
+import Directory from "../../components/direcoty/directory.component";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory></Directory>
+    </div>
+  );
+};
+
+export default Homepage;
